6d07d75eccf8b1bb805f68d47f81ab29b5b1bade
LahiruHarshana <lharshana2002@gmail.com>
2024-04-20 13:47:21 +0530

    added customer and employee in entity layer

---
+++ src/main/java/lk/ijse/gdse66/shoeshop/entity/Customer.java
@@ -0,0 +1,81 @@
+package lk.ijse.gdse66.shoeshop.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * @author : L.H.J
+ * @File: Customer
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-04-20, Saturday
+ **/
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "customer")
+public class Customer {
+    @Id
+    @Column(name = "customer_code")
+    private String customerCode;
+
+    @Column(name = "customer_name")
+    private String customerName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "gender")
+    private Gender gender;
+
+    @Column(name = "join_date")
+    @Temporal(TemporalType.DATE)
+    private Date joinDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "level")
+    private LoyaltyLevel level;
+
+    @Column(name = "total_points")
+    private Integer totalPoints;
+
+    @Column(name = "dob")
+    @Temporal(TemporalType.DATE)
+    private Date dob;
+
+    @Column(name = "address_line_01")
+    private String addressLine1;
+
+    @Column(name = "address_line_02")
+    private String addressLine2;
+
+    @Column(name = "address_line_03")
+    private String addressLine3;
+
+    @Column(name = "address_line_04")
+    private String addressLine4;
+
+    @Column(name = "address_line_05")
+    private String addressLine5;
+
+    @Column(name = "contact_no")
+    private String contactNo;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "recent_purchase_date_time")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date recentPurchaseDateTime;
+
+    enum Gender {
+        MALE, FEMALE, OTHER
+    }
+
+    enum LoyaltyLevel {
+        GOLD, SILVER, BRONZE, NEW
+    }
+}
---
+++ src/main/java/lk/ijse/gdse66/shoeshop/entity/Employee.java
@@ -0,0 +1,77 @@
+package lk.ijse.gdse66.shoeshop.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * @author : L.H.J
+ * @File: Employee
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-04-20, Saturday
+ **/
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @Column(length = 20)
+    private String employee_id;
+
+    @Column(length = 25)
+    private String employeeCode;
+
+    @Column(length = 50)
+    private String employeeName;
+
+    private String employeeProfilePic;
+
+    @Column(length = 10)
+    private String gender;
+
+    @Column(length = 20)
+    private String status;
+
+    @Column(length = 50)
+    private String designation;
+
+    @Column(length = 10)
+    private String accessRole; // ENUM: ADMIN, USER
+
+    private Date dob;
+
+    private Date dateOfJoin;
+
+    @Column(length = 50)
+    private String attachedBranch;
+
+    @Column(length = 50)
+    private String addressLine01; // Preferred building number or name
+
+    @Column(length = 50)
+    private String addressLine02; // Preferred lane
+
+    @Column(length = 50)
+    private String addressLine03; // Main city
+
+    @Column(length = 50)
+    private String addressLine04; // Main state
+
+    @Column(length = 15)
+    private String addressLine05; // Postal code
+
+    @Column(length = 15)
+    private String contactNo; // Mobile number
+
+    @Column(length = 50)
+    private String email;
+
+    @Column(length = 50)
+    private String emergencyContact; // Name of guardian or nominated person for emergency contact
+
+}
