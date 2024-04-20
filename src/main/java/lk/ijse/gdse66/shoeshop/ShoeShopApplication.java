d906dcd5f53a2bc7d61e6653cc197ee2e025f6ba
LahiruHarshana <lharshana2002@gmail.com>
2024-04-20 12:59:49 +0530

    first commit

---
+++ src/main/java/lk/ijse/gdse66/shoeshop/ShoeShopApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse66.shoeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoeShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/gdse66/shoeshop/ShoeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse66.shoeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
