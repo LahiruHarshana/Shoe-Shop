82a180396e84de18ab5dcd59e43f22f23be736de
LahiruHarshana <lharshana2002@gmail.com>
2024-04-20 14:07:05 +0530

    added user in entity layer

---
+++ src/main/java/lk/ijse/gdse66/shoeshop/entity/AdminPanel.java
@@ -0,0 +1,47 @@
+package lk.ijse.gdse66.shoeshop.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @author : L.H.J
+ * @File: AdminPanel
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-04-20, Saturday
+ **/
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "admin_panel")
+public class AdminPanel {
+
+    @Id
+    private String admin_id;
+
+    @Column(name = "total_sales")
+    private Double totalSales;
+
+    @Column(name = "total_profit")
+    private Double totalProfit;
+
+    @Column(name = "most_sale_item")
+    private String mostSaleItem;
+
+    @Lob
+    @Column(name = "most_sale_item_picture")
+    private String mostSaleItemPicture;
+
+    @Column(name = "most_sale_item_qty")
+    private Integer mostSaleItemQuantity;
+
+    public static class AdminPanelPK implements Serializable {
+        private String id;
+    }
+
+}
