955f38326cb3985d5054fee694cb62241e341410
LahiruHarshana <lharshana2002@gmail.com>
2024-04-23 22:23:13 +0530

    creating sale details v1

---
+++ shoe-shop-backend/src/main/java/lk/ijse/gdse66/shoeshopbackend/dto/SaleDetailsDTO.java
@@ -0,0 +1,17 @@
+package lk.ijse.gdse66.shoeshopbackend.dto;
+
+/**
+ * @author : L.H.J
+ * @File: SaleDetailsDTO
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-04-23, Tuesday
+ **/
+public class SaleDetailsDTO {
+    private String orderNo;
+    private String itemCode;
+    private Integer quantity;
+    private Double unitPrice;
+    private String createBy;
+    private String modifyBy;
+    private boolean isActive;
+}
---
+++ shoe-shop-backend/src/main/java/lk/ijse/gdse66/shoeshopbackend/repository/SaleDetailsRepository.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse66.shoeshopbackend.repository;
+
+import lk.ijse.gdse66.shoeshopbackend.entity.SaleDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author : L.H.J
+ * @File: SaleDetailsRepository
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-04-23, Tuesday
+ **/
+public interface SaleDetailsRepository extends JpaRepository<SaleDetails,String> {
+}
