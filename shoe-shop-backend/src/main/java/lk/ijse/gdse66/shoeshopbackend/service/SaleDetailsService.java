b90bb312fb673c52681f24855e2b6746c444d01f
LahiruHarshana <lharshana2002@gmail.com>
2024-04-23 22:25:22 +0530

    creating sale details v1

---
+++ shoe-shop-backend/src/main/java/lk/ijse/gdse66/shoeshopbackend/service/SaleDetailsService.java
@@ -0,0 +1,10 @@
+package lk.ijse.gdse66.shoeshopbackend.service;
+
+/**
+ * @author : L.H.J
+ * @File: SaleDetailsService
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-04-23, Tuesday
+ **/
+public interface SaleDetailsService {
+}
---
+++ shoe-shop-backend/src/main/java/lk/ijse/gdse66/shoeshopbackend/service/impl/SaleDetailsServiceImpl.java
@@ -0,0 +1,15 @@
+package lk.ijse.gdse66.shoeshopbackend.service.impl;
+
+import lk.ijse.gdse66.shoeshopbackend.service.SaleDetailsService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author : L.H.J
+ * @File: SaleDetailsServiceImpl
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-04-23, Tuesday
+ **/
+
+@Service
+public class SaleDetailsServiceImpl implements SaleDetailsService {
+}
