d1073c823fbb5d09e8f3a10721e0b08d09cb8658
LahiruHarshana <lharshana2002@gmail.com>
2024-04-23 20:15:00 +0530

    creating sales crud v1

---
+++ shoe-shop-backend/src/main/java/lk/ijse/gdse66/shoeshopbackend/service/SaleService.java
@@ -0,0 +1,10 @@
+package lk.ijse.gdse66.shoeshopbackend.service;
+
+/**
+ * @author : L.H.J
+ * @File: SaleService
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-04-23, Tuesday
+ **/
+public interface SaleService{
+}
---
+++ shoe-shop-backend/src/main/java/lk/ijse/gdse66/shoeshopbackend/service/impl/SaleServiceImpl.java
@@ -0,0 +1,15 @@
+package lk.ijse.gdse66.shoeshopbackend.service.impl;
+
+import lk.ijse.gdse66.shoeshopbackend.service.SaleService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author : L.H.J
+ * @File: SaleServiceImpl
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-04-23, Tuesday
+ **/
+@Service
+public class SaleServiceImpl implements SaleService {
+    
+}
