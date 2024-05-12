079781714d20f3cefc1ad17dcf4d492b0bc2b32d
LahiruHarshana <lharshana2002@gmail.com>
2024-05-12 17:47:35 +0530

    updated

---
+++ shoe-shop-backend/src/main/java/lk/ijse/gdse66/shoeshopbackend/embedded/Address.java
@@ -0,0 +1,25 @@
+package lk.ijse.gdse66.shoeshopbackend.embedded;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author : L.H.J
+ * @File: Address
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-05-12, Sunday
+ **/
+
+@Data
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+    private String lane;
+    private String mainCountry;
+    private String mainCity;
+    private String mainState;
+    private String postalCode;
+}
