c4a86709ccfeda891e377c49cfa2e41e532e5410
LahiruHarshana <lharshana2002@gmail.com>
2024-05-12 17:48:16 +0530

    updated

---
+++ shoe-shop-backend/src/main/java/lk/ijse/gdse66/shoeshopbackend/embedded/Contact.java
@@ -0,0 +1,20 @@
+package lk.ijse.gdse66.shoeshopbackend.embedded;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author : L.H.J
+ * @File: Contact
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-05-12, Sunday
+ **/
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class Contact {
+}
