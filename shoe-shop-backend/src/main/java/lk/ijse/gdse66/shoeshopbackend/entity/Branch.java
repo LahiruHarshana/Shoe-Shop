ef6914f9a055ce7884d94fd3733d5c52402327f8
LahiruHarshana <lharshana2002@gmail.com>
2024-05-12 17:44:53 +0530

    updated

---
+++ shoe-shop-backend/src/main/java/lk/ijse/gdse66/shoeshopbackend/entity/Branch.java
@@ -0,0 +1,38 @@
+package lk.ijse.gdse66.shoeshopbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+/**
+ * @author : L.H.J
+ * @File: Branch
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-05-12, Sunday
+ **/
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Branch {
+
+    @Id
+    private String branchId;
+    @Column(unique = true)
+    private String branchName;
+    private String branchContact;
+    private String branchManager;
+//    private Address address;
+    private Integer noOfEmployees;
+    @CreationTimestamp
+    private Timestamp createdDate;
+
+    @OneToMany(mappedBy = "branch", cascade = CascadeType.ALL)
+    private List<Employee> employees;
+}
