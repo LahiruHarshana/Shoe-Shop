315d748637a7a95e3297ee34d41b7e58e39a2e2e
LahiruHarshana <lharshana2002@gmail.com>
2024-05-12 20:54:30 +0530

    updated

--- shoe-shop-backend/src/main/java/lk/ijse/gdse66/shoeshopbackend/dto/EmployeeDTO.java
+++ shoe-shop-backend/src/main/java/lk/ijse/gdse66/shoeshopbackend/dto/EmployeeDTO.java
@@ -1,11 +1,15 @@
 package lk.ijse.gdse66.shoeshopbackend.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lk.ijse.gdse66.shoeshopbackend.embedded.Address;
+import lk.ijse.gdse66.shoeshopbackend.enums.Gender;
+import lk.ijse.gdse66.shoeshopbackend.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.sql.Date;
 import java.time.LocalDateTime;
-import java.util.Date;
 
 /**
  * @author : L.H.J
@@ -18,26 +22,22 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 public class EmployeeDTO {
-    private String employeeId;
-    private String employeeCode;
-    private String employeeName;
-    private String employeeProfilePic;
-    private String gender;
-    private String status;
-    private String designation;
-    private String accessRole;
-    private Date dob;
-    private Date dateOfJoin;
-    private String attachedBranch;
-    private String addressLine01;
-    private String addressLine02;
-    private String addressLine03;
-    private String addressLine04;
-    private String addressLine05;
-    private String contactNo;
-    private String email;
+    private String empId;
+    private String empName;
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private Gender gender;
     private String emergencyContact;
-    private String createBy;
-    private String modifyBy;
-    private boolean isActive;
+    private String EmergencyInfo;
+    private String status;
+    private String email;
+    private String contact;
+    private String designation;
+    private Date dob;
+    private Address address;
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private Role role;
+    private String profilePic;
+    private Boolean isActive;
+    private String branchId;
+    private String branchName;
 }
