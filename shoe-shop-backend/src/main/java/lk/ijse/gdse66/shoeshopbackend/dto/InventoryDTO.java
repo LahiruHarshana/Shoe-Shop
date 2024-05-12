b869bf60a5996dcace4aa8956c3373fd564e67b1
LahiruHarshana <lharshana2002@gmail.com>
2024-05-12 20:55:02 +0530

    updated

--- shoe-shop-backend/src/main/java/lk/ijse/gdse66/shoeshopbackend/dto/InventoryDTO.java
+++ shoe-shop-backend/src/main/java/lk/ijse/gdse66/shoeshopbackend/dto/InventoryDTO.java
@@ -1,5 +1,9 @@
 package lk.ijse.gdse66.shoeshopbackend.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lk.ijse.gdse66.shoeshopbackend.enums.ItemGender;
+import lk.ijse.gdse66.shoeshopbackend.enums.ItemStatus;
+import lk.ijse.gdse66.shoeshopbackend.enums.ItemType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,18 +22,25 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class InventoryDTO {
     private String itemCode;
-    private String itemDesc;
+    private String supplierId;
+    private String itemDescription;
     private String itemPicture;
-    private String category;
+    private Integer qtyOnHand;
     private Integer size;
-    private String supplierCode;
-    private String supplierName;
-    private Double unitPriceSale;
-    private Double unitPriceBuy;
+    private Double buyingPrice;
+    private Integer getqty;
+    private String brand;
+    private Double sellingPrice;
     private Double expectedProfit;
+    private Integer itemSoldCount;
+    private Integer getStockTotal;
     private Double profitMargin;
-    private String status;
-    private String createBy;
-    private String modifyBy;
-    private boolean isActive;
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private ItemStatus itemStatus;
+    private Integer discount;
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private ItemType itemType;
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private ItemGender itemGender;
+    private SupplierDTO supplier;
 }
